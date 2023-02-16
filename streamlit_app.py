5aed619028af7abd2d19ef9245cc4556adcc7bed
sfrenubadge2 <125526198+sfrenubadge2@users.noreply.github.com>
2023-02-15 21:36:41 -0600

    Update streamlit_app.py
    fixed the typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale , Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
