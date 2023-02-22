c80bf720c81eb7ac380112d48dee042f37cfcb23
sfrenubadge2 <125526198+sfrenubadge2@users.noreply.github.com>
2023-02-22 15:42:34 -0600

    Update streamlit_app.py
    fixed indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,12 +76,12 @@ if streamlit.button('Get fruit load list'):
 
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute("insert into fruit_load_list values ('from streamlit')")  
-          return "Thanks for adding" + new_fruit
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")  
+         return "Thanks for adding" + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 if streamlit.button('Add a Fruit to the list'):
-     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 back_from_function = insert_row_snowflake(add_my_fruit)
  
 streamlit.write(back_from_function)
