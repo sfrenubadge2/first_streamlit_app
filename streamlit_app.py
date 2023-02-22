9e18a83f55a42d13c3dcaf0cbf4f751e4d5882a2
sfrenubadge2 <125526198+sfrenubadge2@users.noreply.github.com>
2023-02-22 14:26:04 -0600

    Update streamlit_app.py
    fixed indentation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
 except URLError as e:
     streamlit.error()
       
- streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
 
 
 #import requests
